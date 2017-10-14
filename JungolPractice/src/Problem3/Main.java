7984162018b214e0b1513e147e1616b6dc74d061
EANA <EANA@EANA-DESKTOP>
2017-10-15 00:41:42 +0900

    Update Problem 3

--- JungolPractice/src/Problem3/Main.java
+++ JungolPractice/src/Problem3/Main.java
@@ -1,10 +1,68 @@
 package Problem3;
 
+import java.util.Scanner;
+
+/*
+ * The 369 game is a game where you clap the specially number contains 3, 6, 9.
+ * But change the rules of the game so that players clap when the number is a prime number.
+ * 
+ * Input : the number of the participant of the game, the number which is called at the end,
+ * 	and the number of the specific person index
+ * Output : Clap number.
+ * 
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int participantNum = scanner.nextInt();
+		int lastNum = scanner.nextInt();
+		int person = scanner.nextInt();
+		
+		int [] clapNum = runClapNum(participantNum, lastNum);
+		
+		System.out.println(clapNum[person]);
+	}
 
+	private static int[] runClapNum(int participantNum, int lastNum) {
+		
+		int[] result = new int[participantNum];
+		int indexs = 0;
+		
+		for(int i=1; i<=lastNum; i++) {
+			
+			boolean prime = checkPrimeNum(i);
+			
+			if(prime) {
+				result[indexs]++;
+			}
+			
+			if(indexs == participantNum-1) {
+				indexs = 0;
+			}else {
+				indexs++;
+			}
+		}
+		
+		return result;
+	}
+
+	private static boolean checkPrimeNum(int num) {
+		
+		if(num  <= 2) {
+			return true;
+		}
+		
+		for(int i=2; i <= Math.sqrt(num); i++) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 
 }
--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Problem 1. Line up (easy)
 
 Problem 2. Calendar (이미 JAVA에 Calendar 기능이 있는데 어디까지 써야하는지 모르겠다.)
 
-Problem 3. Prime Number 369
+Problem 3. Prime Number 369 (소수인지 아닌지 구분 시간 생각 필수)
