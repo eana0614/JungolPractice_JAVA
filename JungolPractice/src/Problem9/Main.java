c356ca6ee497820a986ecca353a869019b90172d
EANA <EANA@EANA-DESKTOP>
2017-10-16 17:30:38 +0900

    Update Problem 9

--- JungolPractice/src/Problem9/Main.java
+++ JungolPractice/src/Problem9/Main.java
@@ -15,14 +15,14 @@ import java.util.Scanner;
  * 		1+1+1+4
  * 		>> answer : 6
  * 		
- * 
- * Input : T (testcase : 1 <= T <= 64), N (<= 64)
- * 
+ * Input : T (test case : 1 <= T <= 64), N (<= 64)
  * 
  */
 
 public class Main {
 
+	static int [] squares = {1, 2, 4, 8, 16, 32, 64};
+	
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
@@ -32,7 +32,27 @@ public class Main {
 		for(int i=0; i<t; i++) {
 			int n = scanner.nextInt();
 			
+			int count = recursive(0, 0, n);
+			
+			System.out.println(count);
 		}
+	}
+
+	private static int recursive(int sum, int i, int n) {
+		int count = 0;
+		
+		if(sum < n) {
+			for(; i < squares.length; i++) {
+				count += recursive(sum+squares[i], i, n);
+			}
+		}
+		
+		if(sum == n) {
+			count++;
+		}
+		
+		return count;
+
 	}
 
 }
