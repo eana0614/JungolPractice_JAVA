114b74ab49af570b40463e1742823b0649d67625
EANA <EANA@EANA-DESKTOP>
2017-10-16 20:01:07 +0900

    Update Problem 10

--- JungolPractice/src/Problem10/Main.java
+++ JungolPractice/src/Problem10/Main.java
@@ -1,5 +1,6 @@
 package Problem10;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
@@ -17,15 +18,72 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		int n = scanner.nextInt();
+
+		Points[] points = new Points[n];
+
+		while (n > 0) {
+			n--;
+
+			int x = scanner.nextInt();
+			int y = scanner.nextInt();
+
+			points[n] = new Points(x, y);
+		}
+
+		int result = drawAllLine(points);
+		System.out.println(result);
+	}
+
+	private static int drawAllLine(Points[] points) {
+
+		boolean vertical = false;
+		boolean hor = false;
+
+		int count = 0;
 		
-		while(n > 0) {
-			
+		ArrayList<Double> inclination = new ArrayList<>();
+		for (int i = 0; i < points.length - 1; i++) {
+			for (int j = i + 1; j < points.length; j++) {
+
+				double inc;
+				if (points[i].x == points[j].x && points[i].y != points[j].y) {
+					vertical = true;
+				} else if (points[i].x != points[j].x && points[i].y == points[j].y) {
+					hor = true;
+				} else {
+					inc = (double) (points[i].y - points[j].y) / (points[i].x - points[j].x);
+					if (!inclination.contains(inc)) {
+						inclination.add(inc);
+					}
+				}
+			}
 		}
 		
+		count = inclination.size();
+		
+		if(vertical) {
+			count++;
+		}
+		
+		if(hor) {
+			count++;
+		}
+
+		return count;
+	}
+
+	static class Points {
+		int x;
+		int y;
+
+		public Points(int i, int j) {
+			this.x = i;
+			this.y = j;
+		}
 	}
 
 }
